139ca9728ce87a188d65d93703887daf3916e727
Rohit Thakre <thake785@gmail.com>
2022-02-05 13:24:23 +0530

    without top variable code added.

--- Stack/Array/pop.c
+++ Stack/Array/pop.c
@@ -1,4 +1,4 @@
-
+/* //  with top variable 
 int stack_Arr[5], top = -1;
 
 int pop()
@@ -7,4 +7,54 @@ int pop()
     val = stack_Arr[top];
     top--;
     return val;
+} */
+
+// without top variable
+#include <stdio.h>
+#define max 5
+
+int stack_Arr[max], first = -1;
+
+void push(int data)
+{
+    first++;
+    for (int i = first; i > 0; i--)
+        stack_Arr[i] = stack_Arr[i - 1];
+
+    stack_Arr[0] = data;
+}
+
+int pop()
+{
+    int val, i;
+    val = stack_Arr[0];
+
+    for (i = 0; i < first; i++)
+        stack_Arr[i] = stack_Arr[i + 1];
+
+    printf("\nelement deleted successfully.\n");
+    first--;
+    return val;
+}
+
+void print()
+{
+    for (int i = 0; i <= first; i++)
+        printf("%d ", stack_Arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    system("cls");
+    system("color a");
+    push(1);
+    push(2);
+    push(3);
+    print();
+
+    printf("\n%d  deleted.\n", pop());
+    print();
+
+    return 0;
 }
--- Stack/Array/push.c
+++ Stack/Array/push.c
@@ -1,6 +1,44 @@
+/* 
+// using top variable 
+
 int stack_Arr[5], top = -1;
 void push(int data)
 {
     top++;
     stack_Arr[top] = data;
+} */
+
+// without top variable
+
+#include <stdio.h>
+#define max 5
+
+int stack_Arr[max], first = -1;
+
+void push(int data)
+{
+    first++;
+    for (int i = first; i > 0; i--)
+        stack_Arr[i] = stack_Arr[i - 1];
+
+    stack_Arr[0] = data;
+}
+
+void print()
+{
+    for (int i = 0; i <= first; i++)
+        printf("%d ", stack_Arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    system("cls");
+    system("color a");
+    push(1);
+    push(2);
+    push(3);
+    print();
+
+    return 0;
 }
